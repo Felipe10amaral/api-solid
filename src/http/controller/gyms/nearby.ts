ff96bfd8face60e846370ada8a3b167ccd8a9859
Felipe Amaral <felipeamaraldev15@gmail.com>
2023-07-29 22:33:36 -0300

    controller create, search e fetch nearby

---
+++ src/http/controller/gyms/nearby.ts
@@ -0,0 +1,27 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeFetchNearbyGymsUseCase } from '../../../useCases/factories/make-fetch-nearby-gyms-use-case'
+
+export async function nearby(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    latitude: z.number().refine((value) => {
+      return Math.abs(value) <= 90
+    }),
+    longitude: z.number().refine((value) => {
+      return Math.abs(value) <= 180
+    }),
+  })
+
+  const { latitude, longitude } = searchGymsQuerySchema.parse(request.body)
+
+  const nearbyGymsUseCase = makeFetchNearbyGymsUseCase()
+
+  const { gyms } = await nearbyGymsUseCase.execute({
+    userLatitude: latitude,
+    userLongitude: longitude,
+  })
+
+  return reply.status(200).send({
+    gyms,
+  })
+}
